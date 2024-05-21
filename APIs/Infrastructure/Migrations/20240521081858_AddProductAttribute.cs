263b297c10fa126c1df6ce53c2dcf27c3ee8012a
MinhQuan0716 <minhquantrantrinh731@gmail.com>
2024-05-21 15:23:49 +0700

    Add Product Entity

---
+++ APIs/Infrastructure/Migrations/20240521081858_AddProductAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductAttribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductIamgeUrl",
+                table: "Products",
+                newName: "ProductImageUrl");
+
+            migrationBuilder.AddColumn<long>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductPrice",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductImageUrl",
+                table: "Products",
+                newName: "ProductIamgeUrl");
+        }
+    }
+}
